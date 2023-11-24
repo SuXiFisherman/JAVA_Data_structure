e2494c17d21f2018da141e396364f4f9ce420a3a
Sy <111945571+SuXiFisherman@users.noreply.github.com>
2023-11-24 08:10:19 +0800

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,24 +1,22 @@
 public class BinarySearch {
-    public static int BinarySearch(int[] a, int target){
-        int i = 0, j = a.length-1;
+    public static int BinarySearch(int[] a, int target) {
+        int i = 0, j = a.length - 1;
         while (i <= j) {
-            int m = (i+j)/2; //can use (i+j)>>>1
-            if (a[m]==target) {
+            int m = (i + j) / 2; // can use (i+j)>>>1
+            if (a[m] == target) {
                 return m;
-            }
-            else if (a[m]<target){  
-                i = m+1;
-            }
-            else if (a[m]>target) {
-                j = m-1;
+            } else if (a[m] < target) {
+                i = m + 1;
+            } else if (target < a[m]) {
+                j = m - 1;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,5,7,8};
+        int[] a = { 1, 2, 3, 5, 7, 8 };
         int n = BinarySearch(a, 3);
         System.out.println(n);
-}
+    }
 }
